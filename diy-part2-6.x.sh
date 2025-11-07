716d3ddff9e1fd7cacb172b29c04ed3b189a2681
kylin7226 <kylin_fedora@outlook.com>
2025-11-07 20:09:58 +0800

    配置默认网关地址为192.168.199.1

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -11,8 +11,8 @@
 # sed -i "/.*uhttpd.*/d" .config
 # sed -i '/.*\/etc\/init.d.*/d' package/network/services/uhttpd/Makefile
 # sed -i '/.*.\/files\/uhttpd.init.*/d' package/network/services/uhttpd/Makefile
-sed -i "s/:80/:81/g" package/network/services/uhttpd/files/uhttpd.config
-sed -i "s/:443/:4443/g" package/network/services/uhttpd/files/uhttpd.config
+sed -i "s/:80/:64880/g" package/network/services/uhttpd/files/uhttpd.config
+sed -i "s/:443/:64443/g" package/network/services/uhttpd/files/uhttpd.config
 cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # ls package/base-files/files/etc/
 
@@ -20,6 +20,7 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # 追加binder内核参数
 echo "CONFIG_PSI=y
 CONFIG_KPROBES=y" >> target/linux/rockchip/armv8/config-6.6
+
 
 
 # 集成CPU性能跑分脚本
@@ -39,3 +40,7 @@ git clone --depth=1 -b main https://github.com/xiaomeng9597/istoreos-settings pa
 
 # 定时限速插件
 git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
+
+# 修改默认LAN IP地址为192.168.199.1
+sed -i 's/192.168.100.1/192.168.199.1/g' package/base-files/luci2/bin/config_generate 2>/dev/null || true
+echo "✅ 默认LAN IP地址: 192.168.199.1" 

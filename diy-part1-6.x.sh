86ec99dfb0e9f415ca28c3ba98f184dc5afe5124
kylin7226 <kylin_fedora@outlook.com>
2026-06-04 17:27:41 +0800

    优化配置和文档更新
    - 更换 OpenWrt 镜像源为清华大学镜像 (mirrors.tuna.tsinghua.edu.cn)
    - 更新版本更新仓库地址为清华大学镜像
    - 更换自定义软件源为 24.10 版本路径
    - 禁用 homeproxy 及相关依赖包 (sing-box, ucode-mod-digest)
    - 更新 README.md 和工作流发布说明中的访问端口信息 (64880/64443)

--- diy-part1-6.x.sh
+++ diy-part1-6.x.sh
@@ -12,13 +12,13 @@ sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # 修复系统kernel内核md5校验码不正确的问题
+# https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.2/targets/rockchip/armv8/kmods/
 # https://mirrors.ustc.edu.cn/openwrt/releases/24.10.2/targets/rockchip/armv8/kmods/
 # https://downloads.openwrt.org/releases/24.10.2/targets/rockchip/armv8/kmods/
 # https://archive.openwrt.org/releases/24.10.2/targets/rockchip/armv8/kmods/
 # https://mirrors.cqupt.edu.cn/openwrt/releases/24.10.2/targets/rockchip/armv8/kmods/
-# https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/24.10.2/targets/rockchip/armv8/kmods/
 Releases_version=$(cat include/version.mk | sed -n 's|.*releases/\([0-9]\+\.[0-9]\+\.[0-9]\+\).*|\1|p')
-url_value=$(wget -qO- "https://mirrors.ustc.edu.cn/openwrt/releases/${Releases_version}/targets/rockchip/armv8/kmods/")
+url_value=$(wget -qO- "https://mirrors.tuna.tsinghua.edu.cn/openwrt/releases/${Releases_version}/targets/rockchip/armv8/kmods/")
 hash_value=$(echo "$url_value" | sed -n 's/.*6\.6\.93-1-\([0-9a-f]\{32\}\)\/.*/\1/p' | head -1)
 hash_value=${hash_value:-$(echo "$url_value" | sed -n 's/.*\([0-9a-f]\{32\}\)\/.*/\1/p' | head -1)}
 if [ -n "$hash_value" ] && [[ "$hash_value" =~ ^[0-9a-f]{32}$ ]]; then
